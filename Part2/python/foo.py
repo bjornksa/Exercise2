e79fc5ff9ad373e810d02ebcc9ad272012e6353e
Bjorn Kare Sabo <bjornksa@stud.ntnu.no>
2020-01-23 11:37:12 +0100

    Add code to Part2/python and Part2/c

--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -1,21 +1,30 @@
 # Python 3.3.3 and 2.7.6
-# python fo.py
+# python foo.py
 
-from threading import Thread
+from threading import Thread, Lock
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
+mutex = Lock()
 
 def incrementingFunction():
     global i
-    # TODO: increment i 1_000_000 times
+
+    for j in range(100000):
+        mutex.acquire()
+        i += 1
+        mutex.release()
+
 
 def decrementingFunction():
     global i
-    # TODO: decrement i 1_000_000 times
 
+    for j in range(100000):
+        mutex.acquire()
+        i = i - 1
+        mutex.release()
 
 
 def main():
@@ -23,12 +32,13 @@ def main():
 
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
-    
-    # TODO: Start both threads
-    
+
+    incrementing.start()
+    decrementing.start()
+
     incrementing.join()
     decrementing.join()
-    
+
     print("The magic number is %d" % (i))
 
 
